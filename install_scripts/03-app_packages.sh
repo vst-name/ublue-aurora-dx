c09d4293a2250eea71748586d6d7fb83c40a23d6
vst-name <dev@vst.name>
2025-01-23 11:28:31 +0500

    Packages: kdepim-addons and kio-gdrive Unityhub issue unresolved

--- install_scripts/03-app_packages.sh
+++ install_scripts/03-app_packages.sh
@@ -2,6 +2,12 @@
 
 set -ouex pipefail
 
+# Unity Hub
+# sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
+# sudo dnf5 makecache
+
+
+
 dnf5 install -y \
     vlc \
     mangohud \
@@ -24,3 +30,5 @@ dnf5 install -y \
     smb4k \
     kdiff3 \
     gimp
+
+    # unityhub
