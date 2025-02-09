3b8e2ec37e414997f67a48f7693c813d5f8d75ec
vst-name <github@vst.name>
2025-02-10 02:31:47 +0500

    Packages: Removed act-cli

--- install_scripts/01-core_packages.sh
+++ install_scripts/01-core_packages.sh
@@ -4,6 +4,7 @@ set -ouex pipefail
 echo -n "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
 echo -n "fastestmirror=true" >> /etc/dnf/dnf.conf
 
+dnf5 repolist
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/*
 
 dnf5 install -y \
