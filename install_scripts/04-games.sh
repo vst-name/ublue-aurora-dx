b1b9021efcd7d5d05f75d6aa6740d2c8be33a982
vst-name <github@vst.name>
2025-02-11 00:17:08 +0500

    Packages: +Unity +nvidia-container-dev-toolkit fix

--- install_scripts/04-games.sh
+++ install_scripts/04-games.sh
@@ -2,10 +2,6 @@
 
 set -ouex pipefail
 
-Unity Hub
-sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
-rpm --import https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key
-
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf config-manager setopt fedora-cisco-openh264.enabled=1
--- install_scripts/07-dev_packages.sh
+++ install_scripts/07-dev_packages.sh
@@ -19,13 +19,14 @@ dnf5 install -y \
 
 # Unity Hub
 sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
-sudo dnf5 makecache
-yes | dnf5 install -y \
+dnf5 makecache
+rpm --import https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key
+dnf5 install -y \
     unityhub
 
 if [[ ! -f "/etc/yum.repos.d/nvidia-container-toolkit.repo" ]]; then
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo \
-    | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+    | tee /etc/yum.repos.d/nvidia-container-toolkit.repo
     rpm --import "https://nvidia.github.io/libnvidia-container/gpgkey"
     dnf5 install -y nvidia-container-toolkit
 fi
