62bfbaf06f6737adfe1e9b140ae7705ae72279dd
vst-name <github@vst.name>
2025-02-17 15:51:13 +0500

    Packages games.sh fix

--- install_scripts/04-games.sh
+++ install_scripts/04-games.sh
@@ -2,9 +2,8 @@
 
 set -ouex pipefail
 
-dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf config-manager setopt fedora-cisco-openh264.enabled=1
+dnf5 install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
 
 dnf5 install -y \
     mangohud \
