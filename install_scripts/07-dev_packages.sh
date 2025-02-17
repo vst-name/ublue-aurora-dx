0585ec941d00ba1b4883200fed93b37952097aef
vst-name <github@vst.name>
2025-02-17 15:51:13 +0500

    Packages games.sh fix

--- install_scripts/07-dev_packages.sh
+++ install_scripts/07-dev_packages.sh
@@ -17,16 +17,16 @@ dnf5 install -y \
     golang \
     nodejs
 
-# # Unity Hub
-# sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
-# dnf5 makecache
-# rpm --import https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key
-# dnf5 install -y \
-#     unityhub
-
 if [[ ! -f "/etc/yum.repos.d/nvidia-container-toolkit.repo" ]]; then
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo \
     | tee /etc/yum.repos.d/nvidia-container-toolkit.repo
     rpm --import "https://nvidia.github.io/libnvidia-container/gpgkey"
     dnf5 install -y nvidia-container-toolkit
 fi
+
+# # Unity Hub
+sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
+dnf5 makecache
+rpm --import https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key
+dnf5 install -y \
+    unityhub
