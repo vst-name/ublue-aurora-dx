e9eadf86b9c0eacf83225db6c2db0fe1036f25d5
vst-name <github@vst.name>
2025-02-17 14:53:00 +0500

    Packages: -unityhub

--- install_scripts/07-dev_packages.sh
+++ install_scripts/07-dev_packages.sh
@@ -17,12 +17,12 @@ dnf5 install -y \
     golang \
     nodejs
 
-# Unity Hub
-sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
-dnf5 makecache
-rpm --import https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key
-dnf5 install -y \
-    unityhub
+# # Unity Hub
+# sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
+# dnf5 makecache
+# rpm --import https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key
+# dnf5 install -y \
+#     unityhub
 
 if [[ ! -f "/etc/yum.repos.d/nvidia-container-toolkit.repo" ]]; then
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo \
