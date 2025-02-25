e31503f5187089131a1d4a435d402946ec51d971
vst-name <github@vst.name>
2025-02-25 18:15:27 +0500

    Restructured repo enable/disable mechanism

--- install_scripts/01-core_packages.sh
+++ install_scripts/01-core_packages.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 set -ouex pipefail
-echo -n "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
-echo -n "fastestmirror=true" >> /etc/dnf/dnf.conf
 
-dnf5 repolist
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/*
+
+# dnf5 repolist
+# sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/*
 
 dnf5 install -y \
     dnf5-plugins \
