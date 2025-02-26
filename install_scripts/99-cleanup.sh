cf961bf6b56d4acaf3e31802d2ff7ea720d0d13b
vst-name <github@vst.name>
2025-02-26 11:34:06 +0500

    Packages: -Heroic, cleanup

--- install_scripts/99-cleanup.sh
+++ install_scripts/99-cleanup.sh
@@ -3,11 +3,12 @@
 set -eoux pipefail
 shopt -s extglob
 
+
+# Steam 
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=0
+
 dnf5 clean all -y
 dnf5 autoremove -y
 rm -rf /tmp/!(direct_packages|install_scripts) || true
 rm -rf /var/!(cache)
 rm -rf /var/cache/!(rpm-ostree)
-
-# Steam 
-dnf5 config-manager setopt fedora-cisco-openh264.enabled=0
