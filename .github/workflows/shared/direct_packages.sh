88814e9f956475b5b1341f3e2736a24cdfc4bb46
vst-name <github@vst.name>
2025-02-26 11:34:06 +0500

    Packages: -Heroic, cleanup

--- .github/workflows/shared/direct_packages.sh
+++ .github/workflows/shared/direct_packages.sh
@@ -3,7 +3,7 @@ set -eoux pipefail
 mkdir -p ./direct_packages
 (
     gh release download --repo wagoodman/dive --pattern "*linux*amd64*.rpm" -D ./direct_packages/ &
-    gh release download --repo Heroic-Games-Launcher/HeroicGamesLauncher --pattern "*rpm*" -D ./direct_packages/ &
     gh release download --repo kubernetes/minikube --pattern "*latest*x86_64*rpm*" -D ./direct_packages/ &
     wait
 )
+    # gh release download --repo Heroic-Games-Launcher/HeroicGamesLauncher --pattern "*rpm*" -D ./direct_packages/ &
