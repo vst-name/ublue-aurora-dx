64b91d45b421cbc717f7a27e6082ab8bf24fd574
vst-name <github@vst.name>
2025-03-04 01:31:33 +0500

    switched from rpm-fusion to terra-repo for steam added telegram-message actions

--- install_scripts/99-cleanup.sh
+++ install_scripts/99-cleanup.sh
@@ -3,8 +3,8 @@
 set -eoux pipefail
 shopt -s extglob
 
-dnf5 clean all -y
 dnf5 autoremove -y
+dnf5 clean all -y
 rm -rf /tmp/!(direct_packages|install_scripts) || true
 rm -rf /var/!(cache)
 rm -rf /var/cache/!(rpm-ostree)
