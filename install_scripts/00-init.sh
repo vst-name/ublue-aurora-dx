cf26d59bacfc87087d8888b391dd102868ff4dea
Vladislav Stishenko <github@vst.name>
2025-05-01 11:03:51 +0500

    CI Cleanup
    Signed-off-by: Vladislav Stishenko <github@vst.name>

--- install_scripts/00-init.sh
+++ install_scripts/00-init.sh
@@ -9,12 +9,8 @@ dnf5 install -y \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
 
-# dnf5 group upgrade core -y
 dnf5 config-manager setopt terra.enabled=1
 dnf5 config-manager setopt fedora-multimedia.enabled=1
 
-# dnf5 install -y fedora-workstation-repositories
 # dnf5 config-manager setopt google-chrome.enabled=1
 
-#dnf5 group install multimedia -y
-# dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
--- install_scripts/99-cleanup.sh
+++ install_scripts/99-cleanup.sh
@@ -5,5 +5,3 @@ shopt -s extglob
 
 dnf5 autoremove -y
 dnf5 clean all -y
-# rm -rf /var/!(cache)
-# rm -rf /var/cache/!(rpm-ostree)
