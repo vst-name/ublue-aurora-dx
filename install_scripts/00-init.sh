cce83c5e7c4425629981e921a84f977f37db682a
Vladislav Stishenko <github@vst.name>
2025-05-02 10:26:38 +0500

    Updated from ublue template, fmt .sh files
    Signed-off-by: Vladislav Stishenko <github@vst.name>

--- install_scripts/00-init.sh
+++ install_scripts/00-init.sh
@@ -2,15 +2,14 @@
 
 set -ouex pipefail
 
-echo -n "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+echo -n "max_parallel_downloads=10" >>/etc/dnf/dnf.conf
 
 dnf5 install -y \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
+	https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
+	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
 
 dnf5 config-manager setopt terra.enabled=1
 dnf5 config-manager setopt fedora-multimedia.enabled=1
 
 # dnf5 config-manager setopt google-chrome.enabled=1
-
