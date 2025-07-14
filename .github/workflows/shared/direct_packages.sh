e5869cd71cfce51d8d9bcf636442e4139a5daa1b
Vladislav Stishenko <github@vst.name>
2025-07-15 00:55:04 +0500

    Packages: +freelens
    Signed-off-by: Vladislav Stishenko <github@vst.name>

--- .github/workflows/shared/direct_packages.sh
+++ .github/workflows/shared/direct_packages.sh
@@ -3,9 +3,9 @@ set -eoux pipefail
 mkdir -p ./direct_packages
 (
 	wget https://yum.vector.dev/stable/vector-0/"$(uname -m)"/vector-0.46.1-1."$(uname -m)".rpm -q -O ./direct_packages/vector.rpm &
-	wait
+  gh release download --repo freelensapp/freelens --pattern "*amd64*rpm*" -D ./direct_packages/ &
+  wait
 )
-
 # gh release download --repo Heroic-Games-Launcher/HeroicGamesLauncher --pattern "*rpm*" -D ./direct_packages/ &
 # gh release download --repo kubernetes/minikube --pattern "*latest*x86_64*rpm*" -D ./direct_packages/ &
 # gh release download --repo wagoodman/dive --pattern "*linux*amd64*.rpm" -D ./direct_packages/ &
